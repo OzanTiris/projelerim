e3c27b3d5d880af6655a96464668315091d4a138
OzanTiris <101598573+OzanTiris@users.noreply.github.com>
2022-04-28 00:52:52 +0300

    Add files via upload

---
+++ formm 5 quizz 2/formm 5 quizz 2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace formm_5_quizz_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.K = new System.Windows.Forms.Button();
+            this.S = new System.Windows.Forms.Button();
+            this.Y = new System.Windows.Forms.Button();
+            this.SAYAÇ = new System.Windows.Forms.Button();
+            this.SÜRE = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // K
+            // 
+            this.K.BackColor = System.Drawing.Color.Black;
+            this.K.Location = new System.Drawing.Point(187, 12);
+            this.K.Name = "K";
+            this.K.Size = new System.Drawing.Size(112, 85);
+            this.K.TabIndex = 0;
+            this.K.UseVisualStyleBackColor = false;
+            // 
+            // S
+            // 
+            this.S.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.S.Location = new System.Drawing.Point(187, 102);
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(112, 84);
+            this.S.TabIndex = 1;
+            this.S.UseVisualStyleBackColor = false;
+            // 
+            // Y
+            // 
+            this.Y.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Y.Location = new System.Drawing.Point(187, 192);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(112, 85);
+            this.Y.TabIndex = 2;
+            this.Y.UseVisualStyleBackColor = false;
+            // 
+            // SAYAÇ
+            // 
+            this.SAYAÇ.Location = new System.Drawing.Point(146, 283);
+            this.SAYAÇ.Name = "SAYAÇ";
+            this.SAYAÇ.Size = new System.Drawing.Size(203, 116);
+            this.SAYAÇ.TabIndex = 3;
+            this.SAYAÇ.UseVisualStyleBackColor = true;
+            // 
+            // SÜRE
+            // 
+            this.SÜRE.Tick += new System.EventHandler(this.SÜRE_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SAYAÇ);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.S);
+            this.Controls.Add(this.K);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button K;
+        private System.Windows.Forms.Button S;
+        private System.Windows.Forms.Button Y;
+        private System.Windows.Forms.Button SAYAÇ;
+        private System.Windows.Forms.Timer SÜRE;
+    }
+}
+
---
+++ formm 5 quizz 2/formm 5 quizz 2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formm_5_quizz_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int süresay, devir;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            süresay = 30;
+            SÜRE.Enabled = true;
+            SÜRE.Interval = 400;
+            K.Text = "";
+            S.Text = "";
+            Y.Text = "";
+            SAYAÇ.Text = süresay.ToString();
+            K.BackColor = Color.Red;
+            S.BackColor = Color.Black;
+            Y.BackColor = Color.Black;
+            SAYAÇ.BackColor = Color.LightBlue;
+        }
+
+        private void SÜRE_Tick(object sender, EventArgs e)
+        {
+            if (devir == 0)
+            {
+                if (süresay == 2)
+                    S.BackColor = Color.Yellow;
+                if (süresay == 0)
+                {
+                    K.BackColor = Color.Black;
+                    S.BackColor = Color.Black;
+                    Y.BackColor = Color.Green;
+                    süresay = 30;
+                    devir = 1;
+                }
+            }
+            if (devir == 1)
+            {
+                if (süresay == 2)
+                    S.BackColor = Color.Yellow;
+                if (süresay == 0)
+                {
+                    K.BackColor = Color.Red;
+                    S.BackColor = Color.Black;
+                    Y.BackColor = Color.Black;
+                    süresay = 30;
+                    devir = 0;
+                }
+            }
+            SAYAÇ.Text = süresay.ToString();
+            süresay = süresay - 1;
+        }
+    }
+
+   
+}
