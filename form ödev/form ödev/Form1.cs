8d1f5d499ec0018687d61fcc7b172baa4b0ceab6
OzanTiris <101598573+OzanTiris@users.noreply.github.com>
2022-04-28 00:52:02 +0300

    Add files via upload

---
+++ form ödev/form ödev/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_ödev
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            listBox1.Items.Add(textBox1.Text);
+            listBox1.Items.Add(textBox2.Text);
+            listBox1.Items.Add(textBox3.Text);
+            listBox1.Items.Add(textBox4.Text);
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    } 
+}
