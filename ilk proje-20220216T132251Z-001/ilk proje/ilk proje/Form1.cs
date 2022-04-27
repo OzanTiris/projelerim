6e76379ba39e167f3eac4a124477557e64056fb6
OzanTiris <101598573+OzanTiris@users.noreply.github.com>
2022-04-28 00:47:30 +0300

    Add files via upload

---
+++ ilk proje-20220216T132251Z-001/ilk proje/ilk proje/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ilk_proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.White;
+            label1.BackColor = Color.Gray;
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+            label1.Text = "Sinan Engin ile Beyaz Futbol";
+
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Text = "Sinan Engin";
+        }
+    }
+}
