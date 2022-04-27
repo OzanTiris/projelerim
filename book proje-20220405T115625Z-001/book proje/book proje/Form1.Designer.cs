83960191949ab6056b21f16ae0eda8680d46fd35
OzanTiris <101598573+OzanTiris@users.noreply.github.com>
2022-04-28 00:33:38 +0300

    Add files via upload

---
+++ book proje-20220405T115625Z-001/book proje/book proje/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace book_proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btnlist = new System.Windows.Forms.Button();
+            this.txtkitapıd = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btndelete = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btnlist
+            // 
+            this.Btnlist.Location = new System.Drawing.Point(101, 197);
+            this.Btnlist.Name = "Btnlist";
+            this.Btnlist.Size = new System.Drawing.Size(195, 48);
+            this.Btnlist.TabIndex = 49;
+            this.Btnlist.Text = "Listele";
+            this.Btnlist.UseVisualStyleBackColor = true;
+            this.Btnlist.Click += new System.EventHandler(this.Btnlist_Click);
+            // 
+            // txtkitapıd
+            // 
+            this.txtkitapıd.Location = new System.Drawing.Point(101, 12);
+            this.txtkitapıd.Name = "txtkitapıd";
+            this.txtkitapıd.Size = new System.Drawing.Size(240, 20);
+            this.txtkitapıd.TabIndex = 48;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 47;
+            this.label8.Text = "Kitap Id:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(383, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(405, 179);
+            this.dataGridView1.TabIndex = 46;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(101, 94);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(240, 20);
+            this.txtPrice.TabIndex = 45;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 44;
+            this.label7.Text = "Fiyat:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(101, 143);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(195, 48);
+            this.btnSave.TabIndex = 43;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(101, 68);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(240, 20);
+            this.txtAuthor.TabIndex = 42;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Yazar:";
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Location = new System.Drawing.Point(101, 42);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(240, 20);
+            this.txtBookName.TabIndex = 38;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Kitap Adı:";
+            // 
+            // Btndelete
+            // 
+            this.Btndelete.Location = new System.Drawing.Point(101, 251);
+            this.Btndelete.Name = "Btndelete";
+            this.Btndelete.Size = new System.Drawing.Size(195, 48);
+            this.Btndelete.TabIndex = 50;
+            this.Btndelete.Text = "Sil";
+            this.Btndelete.UseVisualStyleBackColor = true;
+            this.Btndelete.Click += new System.EventHandler(this.Btndelete_Click);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Location = new System.Drawing.Point(101, 305);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(195, 48);
+            this.BtnUpdate.TabIndex = 51;
+            this.BtnUpdate.Text = "Güncelle";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(398, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 80);
+            this.groupBox1.TabIndex = 52;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.Btndelete);
+            this.Controls.Add(this.Btnlist);
+            this.Controls.Add(this.txtkitapıd);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btnlist;
+        private System.Windows.Forms.TextBox txtkitapıd;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btndelete;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ book proje-20220405T115625Z-001/book proje/book proje/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace book_proje
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=dbcizgi;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnlist_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("select * from TblBook", connection);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into TblBook (BookName,BookAuthor,BookPrice) values (@p1,@p2,@p3)", connection);
+       
+            command.Parameters.AddWithValue("@p1", txtBookName.Text);
+            command.Parameters.AddWithValue("@p2", txtAuthor.Text);
+            command.Parameters.AddWithValue("@p3", txtPrice.Text);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Kitap Eklendi");
+        }
+
+        private void Btndelete_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("Delete From TblBook Where BookID=@p1", connection);
+            command.Parameters.AddWithValue("@p1", txtkitapıd.Text);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Kayıt Silindi");
+
+            connection.Close();
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("update TblBook set BookName=@p1,BookAuthor=@p3,BookPrice=@p4 where BookID=@p2", connection);
+            command.Parameters.AddWithValue("@p1", txtBookName.Text);
+            command.Parameters.AddWithValue("@p2", txtkitapıd.Text);
+            command.Parameters.AddWithValue("@p3", txtAuthor.Text);
+            command.Parameters.AddWithValue("@p4", txtPrice.Text);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Kayıt Güncellendi");
+            connection.Close();
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
