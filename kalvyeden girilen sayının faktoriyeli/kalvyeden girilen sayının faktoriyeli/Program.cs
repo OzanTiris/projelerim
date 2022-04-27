1cfc352dd7f5d4d77ca10dd9ce2a6e2a33477bdd
OzanTiris <101598573+OzanTiris@users.noreply.github.com>
2022-04-28 00:38:44 +0300

    Add files via upload

---
+++ kalvyeden girilen sayının faktoriyeli/kalvyeden girilen sayının faktoriyeli/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace kalvyeden_girilen_sayının_faktoriyeli
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            int sayi;
+            int f = 1;
+            Console.WriteLine("sayı giriniz:");
+              sayi = Convert.ToInt32(Console.ReadLine());
+           
+            while (sayi>1)
+            {
+                f = f * sayi;
+                sayi--;
+            }
+            Console.WriteLine(f);
+
+
+        }
+    }
+}
